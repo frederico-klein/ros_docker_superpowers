a5d9466bde7283c35600c428c63460ea6a1b89cf
fbk <frederico.klein@plymouth.ac.uk>
2021-01-06 01:42:52 +0000

    squelch errors

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,2 +1,2 @@
-docker container rm $(docker container ls -aq)
-docker volume rm $(docker volume ls -qf dangling=true)
+docker container rm $(docker container ls -aq) 2>/dev/null
+docker volume rm $(docker volume ls -qf dangling=true) 2>/dev/null
